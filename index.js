f9a6a8ee7939bd9f98b1be3763aa75a6ea4d4423
Austin Kregel <austin@kbcomp.co>
2019-06-23 03:01:14 -0400

    Add option for custom host

--- index.js
+++ index.js
@@ -1,91 +1,93 @@
-const winston = require('winston');
-const {format} = winston;
-const fs = require('fs');
-const path = require('path');
-const LOG_DIR = '/var/log/basement-agent';
-const ETC_DIR = '/etc/basement/';
-const CONFIG_PATH = '/etc/basement/config.json';
-const SONAR_HOST = 'http://basement.test/api/metrics/';
-const si = require('systeminformation');
-const axios = require('axios');
-const io = require('socket.io')(4000, {origins: '*:*'});
+const winston = require("winston");
+const { format } = winston;
+const fs = require("fs");
+const path = require("path");
+const LOG_DIR = "/var/log/basement-agent";
+const homedir = require("os").homedir();
+const ETC_DIR = path.join(homedir, ".config/basement");
+const CONFIG_PATH = path.join(homedir, ".config/basement/config.json");
+
+const SONAR_HOST = (process.env.API_HOST ? process.env.API_HOST : "http://basement.test") + "/api/metrics/";
+const si = require("systeminformation");
+const axios = require("axios");
+const io = require("socket.io")(4000, { origins: "*:*" });
+
 io.origins((origin, callback) => {
     callback(null, true);
 });
 
-
 if (!fs.existsSync(ETC_DIR) || !fs.existsSync(CONFIG_PATH)) {
     if (!process.env.API_TOKEN) {
-        console.error('You MUST run this command with the API_TOKEN env set on the first run!');
-        console.error('API_TOKEN=YOUR_API_TOKEN node index.js');
+        console.error("You MUST run this command with the API_TOKEN env set on the first run!");
+        console.error("API_TOKEN=YOUR_API_TOKEN node index.js");
         process.exit(1);
     }
 
     try {
         fs.mkdirSync(ETC_DIR);
-    } catch (e) {
-    }
+    } catch (e) {}
 
     let config = {
-        uuid: require('uuid/v4')(),
-        token: process.env.API_TOKEN
+        uuid: require("uuid/v4")(),
+        token: process.env.API_TOKEN,
     };
 
     try {
         fs.writeFileSync(CONFIG_PATH, JSON.stringify(config));
-    } catch (e) {
-    }
+    } catch (e) {}
 
-
-    axios.post(SONAR_HOST + 'register', {
-        uuid: config.uuid
-    }, {
-        headers: {
-            'Authorization': 'Bearer ' + config.token
-        }
-    }).then(({data}) => {
-        console.log(data)
-    }).catch(({response: {data}}) => {
-        console.log(data)
-    })
-
+    axios
+        .post(
+            SONAR_HOST + "register",
+            { uuid: config.uuid },
+            {
+                headers: {
+                    Authorization: "Bearer " + config.token,
+                },
+            }
+        )
+        .then(({ data }) => {
+            console.log(data);
+        })
+        .catch(({ response: { data } }) => {
+            console.log(data);
+        });
 }
 
 try {
     fs.mkdirSync(LOG_DIR);
-} catch (e) {
-}
+} catch (e) {}
 
 const config = require(CONFIG_PATH);
 
-const cpuProvider = require('./Providers/CPUProvider');
-const ramProvider = require('./Providers/MemoryProvider');
-const diskProvider = require('./Providers/DiskProvider');
+const cpuProvider = require("./Providers/CPUProvider");
+const ramProvider = require("./Providers/MemoryProvider");
+const diskProvider = require("./Providers/DiskProvider");
 
 const load = (provider, name, accessor) => () => {
-    provider.loadData().then((data) => {
-        io.sockets.in('server.' + config.uuid)
-            .emit(name, accessor ? data[accessor] : data)
-    }).catch(e => console.error(e));
+    provider
+        .loadData()
+        .then(data => {
+            io.sockets.in("server." + config.uuid).emit(name, accessor ? data[accessor] : data);
+        })
+        .catch(e => console.error(e));
+};
 
-}
-
-const getRamData = load(ramProvider, 'ram');
-const getCpuData = load(cpuProvider, 'cpu', 'currentload');
-const getDiskData = load(diskProvider, 'disk');
-io.on('connection', function (socket) {
-    socket.join('server.' + config.uuid);
-
+const getRamData = load(ramProvider, "ram");
+const getCpuData = load(cpuProvider, "cpu", "currentload");
+const getDiskData = load(diskProvider, "disk");
+io.on("connection", function(socket) {
+    socket.join("server." + config.uuid);
 
     let cpuInterval = setInterval(getCpuData, 2500);
-    let ramInterval = setInterval(getRamData,2500);
-    let diskInterval = setInterval(getDiskData,2500);
+    let ramInterval = setInterval(getRamData, 2500);
+    let diskInterval = setInterval(getDiskData, 2500);
     getRamData();
     getCpuData();
-    getDiskData()
-    socket.on('disconnect', () => {
+    getDiskData();
+    socket.on("disconnect", () => {
         clearInterval(ramInterval);
         clearInterval(cpuInterval);
         clearInterval(diskInterval);
-    })
+    });
 });
